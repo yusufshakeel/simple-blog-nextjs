25f9282de3f21d01d2359d4fc61fe673f5f9b0d3
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2022-10-24 13:30:33 +0530

    404 update

--- pages/404.js
+++ pages/404.js
@@ -3,10 +3,10 @@ import Link from "next/link"
 export default function NotFound404Page() {
     return (
         <div className="container">
-            <div className="row my-3 text-center">
+            <div className="row my-5 p-5 text-center">
                 <h1 className="display-1">404</h1>
                 <h2 className="display-6">Not found</h2>
-                <p><Link href='/'>Back to home</Link></p>
+                <p><Link href='/'><button className='btn btn-lg btn-primary'>Back to home</button></Link></p>
             </div>
         </div>
     )
